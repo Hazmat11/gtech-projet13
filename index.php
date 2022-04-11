948aa09d9e5af7f91b05f1ad123735558ba0d920
Hazmat11 <ebertrand@gaming.tech>
2022-04-11 12:44:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,10 @@
 <html lang="en" dir="ltr">
 
 <head>
-  <link type="text/css" rel="stylesheet" href="css/materialize.css" media="screen,projection" />
-  <link rel="stylesheet" type="text/css" href="css/style.css">
-    <!--Let browser know website is optimized for mobile-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+  <link type="text/css" rel="stylesheet" href="css/index.css" media="screen,projection" />
   <meta charset="utf-8">
+  <script src="js/script.js" charset="utf-8"></script>
   <title></title>
 </head>
 
@@ -18,35 +17,54 @@
         <li><a href="sass.html">Sass</a></li>
         <li><a href="badges.html">Components</a></li>
         <li><a href="collapsible.html">JavaScript</a></li>
-        <li><a href="collapsible.html">Panier</a></li>
-        <li><a href="collapsible.html">Connexion</a></li>
       </ul>
     </div>
   </nav>
-  <div class="container">
-    <div class="row">
-      <col md="span : 3">
-      <div class="card" style="max-width: 500px; max-height: 500px;">
+  <h1>la gamebox d'octobre sur le theme halloween</h1>
+  <div class="row">
+    <div class="col">
+      <div class="card" style="max-width: 250px; max-height: 250px;">
         <div class="card-image waves-effect waves-block waves-light">
           <img class="activator" src="img/deadbydaylight.png">
         </div>
-        <div class="card-content">
-          <span class="card-title activator grey-text text-darken-4">Card Title<i
-              class="material-icons right">more_vert</i></span>
-          <p><a href="#">This is a link</a></p>
-        </div>
-        <div class="card-reveal">
-          <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-          <p>Here is some more information about this product that is only revealed once clicked on.</p>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card" style="max-width: 250px; max-height: 250px;">
+        <div class="card-image waves-effect waves-block waves-light">
+          <img class="activator" src="img/deadbydaylight.png">
         </div>
       </div>
-      </col>
+    </div>
+    <div class="col">
+      <div class="card" style="max-width: 250px; max-height: 250px;">
+        <div class="card-image waves-effect waves-block waves-light">
+          <img class="activator" src="img/deadbydaylight.png">
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card" style="max-width: 250px; max-height: 250px;">
+        <div class="card-image waves-effect waves-block waves-light">
+          <img class="activator" src="img/deadbydaylight.png">
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card" style="max-width: 250px; max-height: 250px;">
+        <div class="card-image waves-effect waves-block waves-light">
+          <img class="activator" src="img/deadbydaylight.png">
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card" style="max-width: 250px; max-height: 250px;">
+        <div class="card-image waves-effect waves-block waves-light">
+          <img class="activator" src="img/deadbydaylight.png">
+        </div>
+      </div>
     </div>
   </div>
 </body>
-
-  <script type="text/javascript" src="js/jquery.js"></script>
-  <script type="text/javascript" src="js/materialize.min.js"></script>
-  <script type="text/javascript" src="js/script.js"></script>
 
 </html>
